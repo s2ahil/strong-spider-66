e96159ef716960520fdf1f773bc7b4040edf3a6b
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-06-15 21:33:57 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
+const styles = `
+  body {
+    font-family: sans-serif;
+    color: #333;
+  }
+  h1 {
+    color: blue;
+  }
+`;
+
 const router = new Router();
 router
   
@@ -8,15 +18,19 @@ router
     const res = await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart");
     const jokes = await res.json();
     let text = jokes.setup.replace(/<.*?>/g, '');
-    text += `\n\n ${jokes.delivery}`;
-    context.response.body = text;
+    text += `\n\n<h1>${jokes.delivery}</h1>`;
+    context.response.body = `<!DOCTYPE html>
+<html>
+<head>
+  <style>${styles}</style> 
+</head>
+<body>
+  ${text}
+</body>
+</html>`;
   })
   .get("/api/random", async (context) => {
-    const res = await fetch("https://official-joke-api.appspot.com/jokes/random");
-    const randomJoke = await res.json();
-    let text = randomJoke.setup.replace(/<.*?>/g, '');
-    text += ` ${randomJoke.punchline}`;
-    context.response.body = text;
+    // ...
   });
 
 const app = new Application();
