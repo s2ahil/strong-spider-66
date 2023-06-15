0fe7823a4524fe554244fdf2a79eb11eee55d82c
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-06-15 23:02:58 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import { open, DB } from "https://deno.land/x/sqlite/mod.ts";
-import { existsSync } from "https://deno.land/std/fs/mod.ts";
-
 
 const styles = `
   body {
@@ -23,7 +20,11 @@ const styles = `
 `;
 
 const router = new Router();
-const db = new DB("kv.db");
+
+let jokeData = {
+  joke: "",
+  likes: 0,
+};
 
 async function getRandomJoke() {
   const res = await fetch(
@@ -35,17 +36,16 @@ async function getRandomJoke() {
   return text;
 }
 
+async function updateJokeData() {
+  jokeData.joke = await getRandomJoke();
+  jokeData.likes = 0;
+}
+
+updateJokeData();
+setInterval(updateJokeData, 15000);
+
 router.get("/", async (context) => {
-  let text;
-  const storedJoke = db.query("SELECT value FROM kv WHERE key = 'joke'");
-  if (storedJoke.length > 0) {
-    text = storedJoke[0].value;
-  } else {
-    text = await getRandomJoke();
-    db.query("INSERT INTO kv (key, value) VALUES (?, ?)", ["joke", text]);
-    db.query("INSERT INTO kv (key, value) VALUES (?, ?)", ["likes", 0]);
-  }
-  const likes = db.query("SELECT value FROM kv WHERE key = 'likes'")[0].value;
+  const { joke, likes } = jokeData;
   context.response.body = `<!DOCTYPE html>
     <html>
     <head>
@@ -53,7 +53,7 @@ router.get("/", async (context) => {
       <meta http-equiv="refresh" content="15">
     </head>
     <body>
-      ${text}
+      ${joke}
       <br>
       <button onclick="fetch('/like', { method: 'POST' }).then(() => location.reload())">Like</button>
       <p>Likes: ${likes}</p>
@@ -62,8 +62,7 @@ router.get("/", async (context) => {
 });
 
 router.post("/like", async (context) => {
-  const likes = db.query("SELECT value FROM kv WHERE key = 'likes'")[0].value;
-  db.query("UPDATE kv SET value = ? WHERE key = 'likes'", [likes + 1]);
+  jokeData.likes += 1;
   context.response.status = 200;
 });
 
