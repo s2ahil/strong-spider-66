5ca1409bf72ff52629c12f87769451c429037d03
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-06-15 22:47:49 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import { oakCors } from "https://deno/x/cors@v1.2.2/mod.ts";
-import { open, DB } from "https://deno.land/x/sqlite/mod.ts";
-import { existsSync } from "https://deno.land/std/fs/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+const KV = await Deno.openKv();
 
 const styles = `
   body {
@@ -23,7 +21,7 @@ const styles = `
 `;
 
 const router = new Router();
-const db = new DB("kv.db");
+const kv = new KV("jokes");
 
 async function getRandomJoke() {
   const res = await fetch(
@@ -37,15 +35,15 @@ async function getRandomJoke() {
 
 router.get("/", async (context) => {
   let text;
-  const storedJoke = db.query("SELECT value FROM kv WHERE key = 'joke'");
-  if (storedJoke.length > 0) {
-    text = storedJoke[0].value;
+  const storedJoke = await kv.get("joke");
+  if (storedJoke) {
+    text = storedJoke;
   } else {
     text = await getRandomJoke();
-    db.query("INSERT INTO kv (key, value) VALUES (?, ?)", ["joke", text]);
-    db.query("INSERT INTO kv (key, value) VALUES (?, ?)", ["likes", 0]);
+    await kv.set("joke", text);
+    await kv.set("likes", 0);
   }
-  const likes = db.query("SELECT value FROM kv WHERE key = 'likes'")[0].value;
+  const likes = await kv.get("likes");
   context.response.body = `<!DOCTYPE html>
     <html>
     <head>
@@ -56,14 +54,14 @@ router.get("/", async (context) => {
       ${text}
       <br>
       <button onclick="fetch('/like', { method: 'POST' }).then(() => location.reload())">Like</button>
-      <p>Likes: ${}</p>
+      <p>Likes: ${likes}</p>
     </body>
     </html>`;
 });
 
 router.post("/like", async (context) => {
-  const likes = db.query("SELECT value FROM kv WHERE key = 'likes'")[0].value;
-  db.query("UPDATE kv SET value = ? WHERE key = 'likes'", [likes + 1]);
+  const likes = await kv.get("likes");
+  await kv.set("likes", likes + 1);
   context.response.status = 200;
 });
 
