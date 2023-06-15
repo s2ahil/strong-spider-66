a10ca2ee89d956b05cd183e47a5ebbaf1ce1f72b
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-06-15 20:59:09 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,23 +3,29 @@ import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
 const router = new Router();
 router
-  .get("/", (context) => {
-    context.response.body = "Welcome to joke API!";
-  })
-  .get("/api", async (context) => {
+  // .get("/", (context) => {
+  //   context.response.body = `
+  //     <h1>Welcome to joke API!</h1>
+  //   `;
+  // })
+  .get("/", async (context) => {
     const res = await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit");
-    const jokes = await res.json();
-    context.response.body = jokes;
+    const joke = await res.json();
+    let html = `<h1>Here is a programming joke:</h1>`;
+    html += `<p>${joke.setup} <em>${joke.delivery}</em></p>`;
+    context.response.body = html;
   })
   .get("/api/random", async (context) => {
     const res = await fetch("https://official-joke-api.appspot.com/jokes/random");
     const randomJoke = await res.json();
-    context.response.body = randomJoke;
+    const html = `
+      <h1>Here is a random joke:</h1>
+      <p>${randomJoke.setup} <em>${randomJoke.punchline}</em></p>
+    `;
+    context.response.body = html;
   });
 
 const app = new Application();
-app.use(oakCors()); 
+app.use(oakCors());  
 app.use(router.routes());
-app.use(router.allowedMethods());
-
-await app.listen({ port: 8000 });
+app.use(router
