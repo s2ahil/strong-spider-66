fbf0320d716c16b6f991e33a349cbc0b24f9b0a7
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-06-15 21:15:28 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
-
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
 const router = new Router();
 router
   .get("/", (context) => {
- const res = await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit");
+    context.response.body = "Welcome to joke API!";
+  })
+  .get("/api", async (context) => {
+    const res = await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit");
     const jokes = await res.json();
     context.response.body = jokes;
   })
- 
   .get("/api/random", async (context) => {
     const res = await fetch("https://official-joke-api.appspot.com/jokes/random");
     const randomJoke = await res.json();
@@ -22,4 +23,3 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
-
