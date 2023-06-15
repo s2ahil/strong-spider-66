7f7554f0636beeaa5c05fe49449a0d51fff4e446
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-06-15 22:15:32 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-
 const styles = `
   body {
     background-color:#86EFAC;
