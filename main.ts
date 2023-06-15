0711a0260dc4de47d956e455afd004f0e15bd2e8
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-06-15 20:51:29 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,24 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import data from "./data.json" assert { type: "json" };
 
 const router = new Router();
 router
   .get("/", (context) => {
-    context.response.body = "Welcome to dinosaur API!";
+    context.response.body = "Welcome to joke API!";
   })
-  .get("/api", (context) => {
-    context.response.body = data;
+  .get("/api", async (context) => {
+    const res = await fetch("https://official-joke-api.appspot.com/jokes/random/10");
+    const jokes = await res.json();
+    context.response.body = jokes;
   })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const found = data.find((item) =>
-        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (found) {
-        context.response.body = found;
-      } else {
-        context.response.body = "No dinosaurs found.";
-      }
-    }
+  .get("/api/random", async (context) => {
+    const res = await fetch("https://official-joke-api.appspot.com/jokes/random");
+    const randomJoke = await res.json();
+    context.response.body = randomJoke;
   });
 
 const app = new Application();
-app.use(oakCors()); // Enable CORS for All Routes
+app.use(oakCors()); 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
